2b8d1197ba3cd2b5393dfaa723359a89a8b7ddbf
Samad Musiliu <abdsamad2678@gmail.com>
2022-08-29 17:37:52 +0100

    ...

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -12,28 +12,28 @@ import { VscPaintcan } from 'react-icons/vsc';
 const Sidebar = () => {
     return (
         <div className='sticky l-0 top-0 max-h-screen flex flex-col justify-start sm:px-3 sm:justify-center items-center gap-10 bg-bg-dark-2 w-24 '>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <BsCartFill className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5 bg-icon-bg p-2 sm:p-1 rounded'>
+            <div className='my-4 bg-icon-bg p-2 sm:p-1 rounded'>
                 <GoHome className='h-6 w-6 text-gray-50 cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <VscPaintcan className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <AiOutlinePieChart className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <TbMail className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <RiNotification2Line className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <AiOutlineSetting className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>
-            <div className='my-4 sm:my-5'>
+            <div className='my-4'>
                 <FaSignOutAlt className='h-6 w-6 text-icon-bg cursor-pointer' />
             </div>            
         </div>
